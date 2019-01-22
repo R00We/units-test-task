dcfef166b529e6dd5389808675ed53540565813f
R00We <r00we@yandex.ru>
2019-01-22 22:57:19 +0500

    - Доделываю адаптер групировки по датам - сверстал view для вывода даты в списке Issue

--- app/src/main/java/units/r00we/test_task/ui/GroupByDateAdapter.java
+++ app/src/main/java/units/r00we/test_task/ui/GroupByDateAdapter.java
@@ -8,14 +8,19 @@ import android.support.v7.recyclerview.extensions.AsyncDifferConfig;
 import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeMap;
 
+import units.r00we.test_task.R;
 import units.r00we.test_task.network.Issue;
 import units.r00we.test_task.utils.DateFormatter;
 
@@ -23,6 +28,7 @@ public class GroupByDateAdapter extends RecyclerView.Adapter {
 
     public static final int DATE_HOLDER_TYPE = -1;
     private final PagedListAdapter<Issue, IssueAdapter.IssueViewHolder> wrappedAdapter;
+    private final List<Object> items = new ArrayList<>();
     private final DateFormatter dateFormatter;
 
     public GroupByDateAdapter(PagedListAdapter<Issue, IssueAdapter.IssueViewHolder> wrappedAdapter, DateFormatter dateFormatter) {
@@ -33,28 +39,43 @@ public class GroupByDateAdapter extends RecyclerView.Adapter {
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return wrappedAdapter.onCreateViewHolder(parent, viewType);
+        if (viewType == DATE_HOLDER_TYPE) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.date_view, parent, false);
+            return new DateViewHolder(view);
+        } else {
+            return wrappedAdapter.onCreateViewHolder(parent, viewType);
+        }
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        if (getItemViewType(position) == DATE_HOLDER_TYPE) {
 
-        } else {
-            IssueAdapter.IssueViewHolder issueViewHolder = (IssueAdapter.IssueViewHolder)holder;
-            wrappedAdapter.bindViewHolder(issueViewHolder, position);
+        Object item = items.get(position);
+        if (item != null) {
+            if (getItemViewType(position) == DATE_HOLDER_TYPE) {
+                DateViewHolder dateViewHolder = (DateViewHolder)holder;
+                Date dateItem = (Date)item;
+                dateViewHolder.dateTextView.setText(dateItem.toString());
+            } else {
+                IssueAdapter.IssueViewHolder issueViewHolder = (IssueAdapter.IssueViewHolder)holder;
+                wrappedAdapter.bindViewHolder(issueViewHolder, position);
+            }
         }
-
     }
 
     @Override
     public int getItemCount() {
-        return wrappedAdapter.getItemCount();
+        return items.size();
     }
 
     @Override
     public int getItemViewType(int position) {
-        return wrappedAdapter.getItemViewType(position);
+        Object item = items.get(position);
+        if (item instanceof Date) {
+            return DATE_HOLDER_TYPE;
+        } else {
+            return wrappedAdapter.getItemViewType(position);
+        }
     }
 
     @Override
@@ -68,10 +89,14 @@ public class GroupByDateAdapter extends RecyclerView.Adapter {
     }
 
     private void prepareData(){
+        Log.d("GroupByDateAdapter", "start prepareData");
         PagedList<Issue> pagedList = wrappedAdapter.getCurrentList();
         if (pagedList != null) {
+            Log.d("GroupByDateAdapter", "pagedList size - "+pagedList.size());
+
             TreeMap<Date, Set<Issue>> itemMap = new TreeMap<>();
 
+            items.clear();
 
             for (Issue issue : pagedList) {
                 Date date = dateFormatter.removeTime(issue.getUpdatedAt());
@@ -85,13 +110,18 @@ public class GroupByDateAdapter extends RecyclerView.Adapter {
                 currentSet.add(issue);
             }
 
+            for (Date date : itemMap.keySet()) {
+                items.add(date);
+                items.addAll(itemMap.get(date));
+            }
         }
+        notifyDataSetChanged();
     }
 
 
     @Override
     public void registerAdapterDataObserver(@NonNull RecyclerView.AdapterDataObserver observer) {
-        wrappedAdapter.registerAdapterDataObserver(observer);
+        super.registerAdapterDataObserver(observer);
         wrappedAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
             @Override
             public void onChanged() {
@@ -117,7 +147,7 @@ public class GroupByDateAdapter extends RecyclerView.Adapter {
             @Override
             public void onItemRangeInserted(int positionStart, int itemCount) {
                 super.onItemRangeInserted(positionStart, itemCount);
-                Log.d("GroupByDateAdapter", "onItemRangeInserted");
+                Log.d("GroupByDateAdapter", "onItemRangeInserted positionStart - "+positionStart+" itemCount - "+itemCount);
                 prepareData();
             }
 
@@ -140,9 +170,21 @@ public class GroupByDateAdapter extends RecyclerView.Adapter {
 
     @Override
     public void unregisterAdapterDataObserver(@NonNull RecyclerView.AdapterDataObserver observer) {
-        wrappedAdapter.unregisterAdapterDataObserver(observer);
+        super.unregisterAdapterDataObserver(observer);
+        //todo unregister on wrappedAdapter
 
     }
 
+    static final class DateViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView dateTextView;
+
+        public DateViewHolder(View itemView) {
+            super(itemView);
+            dateTextView = itemView.findViewById(R.id.dateTextView);
+        }
+
+
+    }
 
 }
